d5c4ccb94c430eae6754eb558c1d24b88c534ef7
Paul Obunga <paulobunga.one@gmail.com>
2024-06-22 23:57:45 +0300

    Updated Code: Splash Screen Functional

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 const Splash = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-black relative">
-      <div className="absolute top-0 left-0 w-1/2 h-1/2 bg-gradient-to-br from-white to-transparent rounded-full"></div>
-      <div className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-gradient-to-tl from-white to-transparent rounded-full"></div>
-      <img src="/logo.png" alt="Logo" className="z-10" />
+      <div className="absolute top-0 left-0 bg-gradient-to-br from-white to-transparent rounded-full"
+       style={{ 
+        width: '58vh', 
+        height: '58vh',
+        top: '-25vh',
+        left: '-25vh',
+        overflow: 'clip',
+        opacity: 0.3
+      }}></div>
+      <div className="absolute bottom-0 right-0 bg-gradient-to-tl from-white to-transparent rounded-full"
+      style={{
+        width: '58vh', 
+        height: '58vh',
+        bottom: '-25vh',
+        right: '-25vh',
+       overflow: 'clip',
+       opacity: 0.3
+      }}
+      ></div>
+      <img src="/assets/images/splash_logo.png" alt="Logo" className="z-10" />
     </div>
   );
 };
